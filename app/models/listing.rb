9ae0f4c0a2de8b56bad3b81278d6e26bd7e83e2d
neil patel <neil@myindianweddingapp.com>
2014-11-03 16:51:30 +0000

    Associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
 	validates :name, :description, :price, presence:true
 	validates :price, numericality: { greater_than: 0}
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102131334) do
+ActiveRecord::Schema.define(version: 20141103163259) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141102131334) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
